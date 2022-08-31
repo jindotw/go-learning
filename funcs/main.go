374096d1c07d8f42229fa4c522ec663d7c79a4a7
Paul <jindotw@gmail.com>
2022-08-31 17:15:43 +0800

    feat: added if syntax to check if variable is passed by value or by reference

--- funcs/main.go
+++ funcs/main.go
@@ -6,12 +6,17 @@ type Point struct {
 	x, y float64
 }
 
-func (pt Point) AddX(dx float64) {
+func (pt *Point) AddX(dx float64) {
 	pt.x += dx
 }
 
 func main() {
-	var p1 = &Point{3, 4}
-	(*p1).AddX(2.5)
+	p1 := &Point{3, 4}
+	p1.AddX(2.5)
+	if p1.x > p1.y {
+		fmt.Printf("p1.x (%f) > p1.y (%f)\n", p1.x, p1.y)
+	} else {
+		fmt.Printf("p1.y (%f) > p1.x (%f)\n", p1.y, p1.x)
+	}
 	fmt.Println(p1)
 }
