fe81d3760d3329411abe2ec1a71a8a19e1adf6e6
Paul <jindotw@gmail.com>
2023-06-30 10:39:36 +0800

    feat: implemented PostOrder in iteration

--- tree/btree/post_order.go
+++ tree/btree/post_order.go
@@ -10,3 +10,32 @@ func PostOrderRecur(root *Node) {
 	PostOrderRecur(root.Right)
 	fmt.Println(root.GetValue())
 }
+
+func PostOrderIter(root *Node) {
+	st := make([]*Node, 0, 20)
+	if root != nil {
+		st = append(st, root)
+	}
+
+	for len(st) > 0 {
+		last := len(st) - 1
+		node := st[last]
+		st = st[:last]
+
+		if node.isLeftEmpty() && node.isRightEmpty() {
+			fmt.Println(node.Value)
+		} else {
+			st = append(st, node)
+			if !node.isRightEmpty() {
+				st = append(st, node.Right)
+				node.Right = nil
+			}
+
+			if !node.isLeftEmpty() {
+				st = append(st, node.Left)
+				node.Left = nil
+			}
+		}
+
+	}
+}
