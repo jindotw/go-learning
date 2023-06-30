5fa3bdce14c22f90bce5c5847eb92785838d1460
Paul <jindotw@gmail.com>
2023-06-30 13:51:59 +0800

    feat: feat: implemented PreOrder using common traversal pattern

--- tree/btree/pre_order.go
+++ tree/btree/pre_order.go
@@ -31,3 +31,28 @@ func PreOrderIter(root *Node) {
 		}
 	}
 }
+
+func PreOrderCommon(root *Node) {
+	st := make([]*Node, 0, 10)
+	if root != nil {
+		st = append(st, root)
+	}
+
+	for len(st) > 0 {
+		node := pop(&st)
+		if node != nil {
+			if !node.isRightEmpty() {
+				st = append(st, node.Right)
+			}
+			if !node.isLeftEmpty() {
+				st = append(st, node.Left)
+			}
+
+			st = append(st, node)
+			st = append(st, nil)
+		} else {
+			node = pop(&st)
+			fmt.Println(node.GetValue())
+		}
+	}
+}
