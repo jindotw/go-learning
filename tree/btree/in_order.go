b46baf41ce5fa3d6552a47bfebf142d3129c7e76
Paul <jindotw@gmail.com>
2023-06-30 13:27:29 +0800

    feat: implemented InOrder using iteration

--- tree/btree/in_order.go
+++ tree/btree/in_order.go
@@ -10,3 +10,21 @@ func InOrderRecur(root *Node) {
 	fmt.Println(root.GetValue())
 	InOrderRecur(root.Right)
 }
+
+func InOrderIter(root *Node) {
+	st := make([]*Node, 0, 10)
+	curr := root
+
+	for curr != nil || len(st) > 0 {
+		if curr != nil {
+			st = append(st, curr)
+			curr = curr.Left
+		} else {
+			last := len(st) - 1
+			curr = st[last]
+			st = st[:last]
+			fmt.Println(curr.Value)
+			curr = curr.Right
+		}
+	}
+}
