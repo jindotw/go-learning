6cbb3a7e394f3b28aae240f3aa25d22ee442459e
Paul <jindotw@gmail.com>
2023-09-17 13:29:36 +0800

    feat: added a technique for reversing a slice

--- techniques/reverse.go
+++ techniques/reverse.go
@@ -4,13 +4,21 @@ import "fmt"
 
 func reverseSlice() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7}
-
 	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
 		arr[i], arr[j] = arr[j], arr[i]
 	}
 	fmt.Printf("%#v\n", arr)
 }
 
+func reverseSlice2() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7}
+	for i := 0; i < len(arr)/2; i++ {
+		j := len(arr) - 1 - i
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+	fmt.Printf("%#v\n", arr)
+}
+
 func main() {
-	reverseSlice()
+	reverseSlice2()
 }
